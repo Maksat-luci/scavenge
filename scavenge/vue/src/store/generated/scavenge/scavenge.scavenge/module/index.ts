ca5d5e6a4f12e38851048e14fc055c6f8c6c9b70
“Nurlan” <ikhsan.nurlan@gmail.com>
2022-09-24 21:24:04 +0600

    added: types for CRUD

--- scavenge/vue/src/store/generated/scavenge/scavenge.scavenge/module/index.ts
+++ scavenge/vue/src/store/generated/scavenge/scavenge.scavenge/module/index.ts
@@ -4,14 +4,14 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
-import { MsgRevealSolution } from "./types/scavenge/tx";
 import { MsgSubmitScavenge } from "./types/scavenge/tx";
+import { MsgRevealSolution } from "./types/scavenge/tx";
 import { MsgCommitSolution } from "./types/scavenge/tx";
 
 
 const types = [
-  ["/scavenge.scavenge.MsgRevealSolution", MsgRevealSolution],
   ["/scavenge.scavenge.MsgSubmitScavenge", MsgSubmitScavenge],
+  ["/scavenge.scavenge.MsgRevealSolution", MsgRevealSolution],
   ["/scavenge.scavenge.MsgCommitSolution", MsgCommitSolution],
   
 ];
@@ -45,8 +45,8 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
-    msgRevealSolution: (data: MsgRevealSolution): EncodeObject => ({ typeUrl: "/scavenge.scavenge.MsgRevealSolution", value: MsgRevealSolution.fromPartial( data ) }),
     msgSubmitScavenge: (data: MsgSubmitScavenge): EncodeObject => ({ typeUrl: "/scavenge.scavenge.MsgSubmitScavenge", value: MsgSubmitScavenge.fromPartial( data ) }),
+    msgRevealSolution: (data: MsgRevealSolution): EncodeObject => ({ typeUrl: "/scavenge.scavenge.MsgRevealSolution", value: MsgRevealSolution.fromPartial( data ) }),
     msgCommitSolution: (data: MsgCommitSolution): EncodeObject => ({ typeUrl: "/scavenge.scavenge.MsgCommitSolution", value: MsgCommitSolution.fromPartial( data ) }),
     
   };
